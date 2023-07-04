eb20e93d524d115ab11c6f9b0ced7e5761b16ec5
BRINDA PONKIYA <111215908+Brinda1612@users.noreply.github.com>
2023-07-04 15:37:16 +0530

    day 12

---
+++ php/name.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = "BRINDa PONKIYA";
+$y = 2101031000186;
+$z = "INFORMATION TECHNOLOGY";
+
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+echo "<br>";
+echo $z;
+?>
+
+</body>
+</html>
---
+++ php/operator.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$tex = "Arithmetic Operators";
+$x = 10;  
+$y = 6;
+echo ($tex), "<br>";
+echo ($x + $y), "<br>";
+echo ($x - $y), "<br>";
+echo ($x * $y), "<br>";
+echo ($x / $y), "<br>";
+echo ($x % $y), "<br>";
+
+echo "logical Operators";
+
+?>
+ 
+
+</body>
+</html>
